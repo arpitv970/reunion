814495d9b180f046fd7096657ca665b40372a598
Arpit Verma <arpitv970@gmail.com>
2023-11-24 20:18:23 +0530

    minor fix at url

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const API_URL = 'https://reunion-backend-iqjm.onrender.com';
+export const API_URL = process.env.NEXT_PUBLIC_BACKEND_API;
